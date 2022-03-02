43f7f73f03575b939cf68370ded6d0a3356db12d
bbrandis <76037382+bbrandis@users.noreply.github.com>
2022-03-02 11:41:06 +0100

    Update registerscript.php

--- php/registerscript.php
+++ php/registerscript.php
@@ -1,5 +1,8 @@
 <?php
 
+$validated = true;
+$pwm = true; 
+
 if( !empty($_POST["username"]))
 {
 	echo "Zadal si meno.<br>";
@@ -7,6 +10,7 @@ if( !empty($_POST["username"]))
 else
 {
 	echo "Nezadal si meno.<br>";
+	$validated = false;
 }
 
 if( !empty($_POST["email"]))
@@ -16,6 +20,7 @@ if( !empty($_POST["email"]))
 else
 {
 	echo "Nezadal si mail.<br>";
+	$validated = false;
 }
 
 if( !empty($_POST["password"]))
@@ -25,16 +30,31 @@ if( !empty($_POST["password"]))
 else
 {
 	echo "Nezadal si heslo.<br>";
+	$validated = false;
 }
 
 if ($_POST["password"]!=$_POST["password_check"])
 {
 	echo "Heslá sa nezhodujú.<br>";
+	$validated = false;
+	$pwm = false;
 }
 else
 {
-	echo "Nezadal si heslo znova.<br>";
+	echo "Heslá sa zhodujú.<br>";
 }
+
+$hashed_password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+echo "Unhashed password = " . $_POST["password"];
+echo $hashed_password;
+
+require_once("./Connection.php");
+if($validated && $pwm){
+	$sql = 'INSERT INTO users(password,email,username) VALUES("'. $_POST["password"] .'","'.$_POST["email"] .'","'.$_POST["username"].'");';
+	echo $sql;
+}
+
+
 ?>
 
 
