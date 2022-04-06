c591c2b2efcd8826f98bd78cc7c1954b194b17ac
bbrandis <76037382+bbrandis@users.noreply.github.com>
2022-04-06 08:47:48 +0200

    fotk

--- index.php
+++ index.php
@@ -3,9 +3,37 @@
 	
 	</br>
 	<center>
-	<b><h1>Vitajte!</h1></b><br>xw
-	</div>
+	<b><h1>Vitajte!</h1></b>
 	<br>
 
+<div class="container">
+	 <div id="carouselwithIndicators" class="carousel slide w-50" data-ride="carousel" data-interval="2500">
+		<ol class="carousel-indicators">
+		   <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		   <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		   <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+		   <div class="carousel-item active">
+			  <img class="d-block w-100" src="https://raw.githubusercontent.com/bbrandis/DAA2pl/main/assets/backrgound.jpg" alt="First slide">
+		   </div>
+		   <div class="carousel-item">
+			  <img class="d-block w-100" src="https://raw.githubusercontent.com/bbrandis/DAA2pl/main/assets/linus.png" alt="Second slide">
+		   </div>
+		   <div class="carousel-item">
+			  <img class="d-block w-100" src="https://www.tutorialspoint.com/bootstrap/images/slide3.png" alt="Third slide">
+		   </div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselwithIndicators" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselwithIndicators" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+		</a>
+	 </div>
+  </div>
+	</center>
 	</main>
 <?php include('./php/footer.php') ?>
